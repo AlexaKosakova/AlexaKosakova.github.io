b1cda4e667de656771ec92fec5c603e2eaa6d1eb
AlexaKosakova <34619765+AlexaKosakova@users.noreply.github.com>
2020-10-25 21:21:33 +0300

    Add comments

--- Учебные/Optimisation/ИДЗ/ИДЗ.js
+++ Учебные/Optimisation/ИДЗ/ИДЗ.js
@@ -6,7 +6,7 @@ function calculate() {
 
 
 
-    var func = document.getElementById("func").value
+    var func = document.getElementById("func").value //Анализ и замена данной при вводе функции
         .replace(/\^/g, "**")
         .replace(/([A-Za-wyz][A-Za-z]*)/g, "Math.$1");
     console.log(a, b, func);
@@ -21,7 +21,7 @@ function calculate() {
         }
     }
 
-    function diff(fun, a) {
+    function diff(fun, a) { //Вычисление производной в точке
         h = 0.01;
         epsilon = 0.000001;
         f1 = (fun(a + h) - fun(a)) / h;
@@ -37,11 +37,11 @@ function calculate() {
         return f1;
     }
 
-      function MaxL(fun, a, b) {
+      function MaxL(fun, a, b) { //Поиск максимума производной
         m = diff(fun, a);
         for (i = a; i < b; i = i + 0.01) {
             if (diff(fun, i) > m) {
-                m = diff(fun, i)
+                m = diff(fun, i);
             }
             if (Math.abs(diff(fun, i)) > 1000) {
                 document.getElementById("result").innerHTML = "Эта функция не подходит для вычисления минимума этим методом";
@@ -53,17 +53,17 @@ function calculate() {
 
     }
 
-    L = MaxL(f, a, b);
+    L = MaxL(f, a, b); //L для метода ломанных
 
     if (L == undefined) {
         return;
     }
 
-    function min() {
+    function min() {//Начало метода ломанных
         console.log("Зашел в минимум")
         var t = [];
-        var xMain = 1 / (2 * L) * (f(a) - f(b) + L * (a + b));
-        var pMain = 1 / 2 * (f(a) + f(b) + L * (a - b));
+        var xMain = 1 / (2 * L) * (f(a) - f(b) + L * (a + b)); // Вычисление
+        var pMain = 1 / 2 * (f(a) + f(b) + L * (a - b));          // первых точек
         if (xMain < a || xMain > b || Object.is(NaN, xMain)) {
             xMain = (b - a) / 2;
             pMain = f(xMain);
@@ -71,8 +71,7 @@ function calculate() {
         list.push([xMain, pMain])
         while (true) {
             t = 0;
-            for (i = 0; i < list.length; i++) {
-                console.log("Проходим по всем точкам, i, xMain, pMain ", i, list[i][0], list[i][1])
+            for (i = 0; i < list.length; i++) {//Поиск минимального p среди имеющихся
                 if ((list[i][1]) < (list[t][1])) {
                     t = i;
                 }
@@ -83,13 +82,13 @@ function calculate() {
                 var k = 0;
                 pMain = list[t][1];
                 xMain = list[t][0];
-                var del = 1 / (2 * L) * (f(xMain) - pMain);
-                var x1 = xMain - del;
-                var x2 = xMain + del;
-                p = 1 / 2 * (f(xMain) + pMain)
-                if ((x1 < a || x1 > b || Object.is(NaN, x1)) && (x2 < a || x2 > b || Object.is(NaN, x2))) {
-                    list.splice(t, 1);
-                } else if ((x1 < a || x1 > b || Object.is(NaN, x1)) && x2 > a && x2 < b) {
+                var del = 1 / (2 * L) * (f(xMain) - pMain); // дельта
+                var x1 = xMain - del; // x1'
+                var x2 = xMain + del; // x1''
+                p = 1 / 2 * (f(xMain) + pMain); // p
+                if ((x1 < a || x1 > b || Object.is(NaN, x1)) && (x2 < a || x2 > b || Object.is(NaN, x2))) { // Выбор какие точки добавить:
+                    list.splice(t, 1);                                                                      // если они не входят в данный
+                } else if ((x1 < a || x1 > b || Object.is(NaN, x1)) && x2 > a && x2 < b) {                  // отрезок, то исключаем
                     list.splice(t, 1, [x2, p]);
                 } else if ((x2 < a || x2 > b || Object.is(NaN, x2)) && x1 > a && x1 < b) {
                     list.splice(t, 1, [x1, p]);
@@ -105,18 +104,19 @@ function calculate() {
                 if (k == 0) {
                     break;
                 }
-                if (Math.abs(2 * L * del) < eps) {
+                if (Math.abs(2 * L * del) < eps) { // Проверка на окончание
                     break;
                 }
             }
-            if (Math.abs(2 * L * del) < eps) {
+            if (Math.abs(2 * L * del) < eps) { // Проверка на окончание
                 break;
             }
 
         }
         document.getElementById("result").innerHTML = "Минимум в точке: " + " " + xMain;
-        return [xMain, pMain];
+        return [xMain, pMain]; // Минимум. Конец метода ломанных
     }
+    //Отображение графика
     var trace0 = {
         x: [],
         y: [],
@@ -142,15 +142,15 @@ function calculate() {
         }
     };
 
-    for (let t = a; t < b; t = t + 0.01) {
+    for (let t = a; t < b; t = t + 0.01) { // Отображение графика функций
         trace0.x.push(t);
         trace0.y.push(f(t));
     }
-    for (let t = 0; t < list.length; t++) {
+    for (let t = 0; t < list.length; t++) { // Отображение точек метода
         trace1.x.push(list[t][0]);
         trace1.y.push(list[t][1]);
     }
-    trace2.x.push(min()[0]);
+    trace2.x.push(min()[0]); // Отображение точки минимума
     trace2.y.push(min()[1]);
 
 
